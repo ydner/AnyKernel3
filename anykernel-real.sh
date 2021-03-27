a1cf574158a6bff1a6223f415145d4a3133adf0a
ydner <76888490+ydner@users.noreply.github.com>
2021-03-28 00:45:18 +0700

    Update anykernel-real.sh

--- anykernel-real.sh
+++ anykernel-real.sh
@@ -4,10 +4,9 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Scape
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=joyeuse
@@ -15,12 +14,10 @@ device.name2=curtana
 device.name3=excalibur
 device.name4=gram
 device.name5=
-supported.versions=
-supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/soc/c0c4000.sdhci/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
@@ -32,8 +29,9 @@ ramdisk_compression=auto;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 755 755 $ramdisk/init* $ramdisk/sbin;
+chmod -R 750 $ramdisk/*;
+chmod -R 755 $ramdisk/sbin;
+chown -R root:root $ramdisk/*;
 
 
 ## AnyKernel install
@@ -41,48 +39,40 @@ dump_boot;
 
 # begin ramdisk changes
 
-#Remove old kernel stuffs from ramdisk
-ui_print "cleaning up..."
- rm -rf $ramdisk/init.special_power.sh
- rm -rf $ramdisk/init.darkonah.rc
- rm -rf $ramdisk/init.spectrum.rc
- rm -rf $ramdisk/init.spectrum.sh
- rm -rf $ramdisk/init.boost.rc
- rm -rf $ramdisk/init.trb.rc
- rm -rf $ramdisk/init.azure.rc
- rm -rf $ramdisk/init.PBH.rc
- rm -rf $ramdisk/init.Pbh.rc
- rm -rf $ramdisk/init.overdose.rc
- rm -rf $ramdisk/init.infinity.rc
- rm -rf $ramdisk/init.predator.rc
- rm -rf $ramdisk/init.error.rc
- rm -rf $ramdisk/init.venus.rc
- rm -rf $ramdisk/init.spectrum.rc
-
-backup_file init.rc;
-remove_line init.rc "import /init.darkonah.rc";
-remove_line init.rc "import /init.spectrum.rc";
-remove_line init.rc "import /init.boost.rc";
-remove_line init.rc "import /init.trb.rc"
-remove_line init.rc "import /init.azure.rc"
-remove_line init.rc "import /init.PbH.rc"
-remove_line init.rc "import /init.Pbh.rc"
-remove_line init.rc "import /init.overdose.rc"
-remove_line init.rc "import /init.infinity.rc"
-remove_line init.rc "import /init.predator.rc"
-remove_line init.rc "import /init.error.rc"
-remove_line init.rc "import /init.venus.rc"
-remove_line init.rc "import /init.spectrum.rc"
-
-# Set Android version for kernel
-ver="$(file_getprop /system/build.prop ro.build.version.release)"
-if [ ! -z "$ver" ]; then
-  patch_cmdline "androidboot.version" "androidboot.version=$ver"
-else
-  patch_cmdline "androidboot.version" ""
-fi
-
 # end ramdisk changes
+
+# Uclamp tunables
+if [ -f /dev/cpuset/top-app/uclamp.max ]; then
+	ui_print "  • Uclamp supported kernel"
+	#Uclamp tuning
+	sysctl -w kernel.sched_util_clamp_min_rt_default=96
+	sysctl -w kernel.sched_util_clamp_min=128
+
+	#top-app
+	echo max > /dev/cpuset/top-app/uclamp.max
+	echo 10  > /dev/cpuset/top-app/uclamp.min
+	echo 1   > /dev/cpuset/top-app/uclamp.boosted
+	echo 1   > /dev/cpuset/top-app/uclamp.latency_sensitive
+
+	#foreground
+	echo 50 > /dev/cpuset/foreground/uclamp.max
+	echo 0  > /dev/cpuset/foreground/uclamp.min
+	echo 0  > /dev/cpuset/foreground/uclamp.boosted
+	echo 0  > /dev/cpuset/foreground/uclamp.latency_sensitive
+
+	#background
+	echo max > /dev/cpuset/background/uclamp.max
+	echo 20  > /dev/cpuset/background/uclamp.min
+	echo 0   > /dev/cpuset/background/uclamp.boosted
+	echo 0   > /dev/cpuset/background/uclamp.latency_sensitive
+
+	#system-background
+	echo 40 > /dev/cpuset/system-background/uclamp.max
+	echo 0  > /dev/cpuset/system-background/uclamp.min
+	echo 0  > /dev/cpuset/system-background/uclamp.boosted
+	echo 0  > /dev/cpuset/system-background/uclamp.latency_sensitive
+
+fi
 
 write_boot;
 ## end install
